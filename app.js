c7339568de876befa8cd2911e911d9196a65536c
TonyNguyen101 <anthonylenguyen04@gmail.com>
2015-06-22 17:24:49 -0700

    Have index page for search and skeleton for individual recipe show page

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var express 					= require('express'),
     methodOverride 		= require("method-override"),
     session 					= require("cookie-session"),
     morgan 						= require("morgan"),
-    db 								= require("./models"),
-    request						= require("request"),
-    loginMiddleware 	= require("./middleware/loginHelper"),
+    /*db 								= require("./models"),*/
+    request						= require("request");
+/*    loginMiddleware 	= require("./middleware/loginHelper"),
     routeMiddleware 	= require("./middleware/routeHelper");
-    require('dotenv').load();
-
+    require('dotenv').load();*/
 
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
@@ -18,8 +17,33 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 //ROOT - All recipes, no login
+app.get('/', function (req, res){
+	res.redirect('/recipes');
+});
 
 //INDEX
+app.get('/recipes', function (req, res){
+	var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&title_kw=";
+	var searchTerm = "salmon";
+	var apiKey = "&api_key=dvxK85et7PRw0l0hH7I3D9R2cFMvdPop";
+	request({
+		type: 'GET',
+		json: true,
+		uri: url + searchTerm + apiKey
+	}, 
+	function (err, response, body){
+		if (err) {
+	    console.log("Error! Request failed - " + err);
+	  } else if (!err && response.statusCode === 200) {
+	  	var bigOvenDataArray = body;
+	  	res.render('recipes/index', {bigOvenDataArray:bigOvenDataArray});
+	  	/*res.send(bigOvenDataArray);*/
+	  }
+	});  
+});	
+
+
+
 
 //NEW
 
