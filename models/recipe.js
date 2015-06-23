9a3082c87f6bd4659c6386bc3fd812c60206e5b7
TonyNguyen101 <anthonylenguyen04@gmail.com>
2015-06-22 18:21:26 -0700

    made recipe model

---
+++ models/recipe.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var recipeSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	ingredients: [{
+		unit: String,
+		quantity: Number,
+		name: String		
+	}],
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User" 
+	},
+	comments: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Comment" 
+	}]
+});
+
+var Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
