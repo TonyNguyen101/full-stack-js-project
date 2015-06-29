61646de2f38649b5c1be662cadecdcb9d70eaa46
TonyNguyen101 <anthonylenguyen04@gmail.com>
2015-06-29 16:41:15 -0700

    initial heroku push

--- app.js
+++ app.js
@@ -238,6 +238,6 @@ app.get('*', function(req,res){
   res.render('errors/404');
 });
 
-app.listen(process.env.HOST || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is listening on port 3000");
 });
